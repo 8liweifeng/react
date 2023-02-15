278868b6c9436f222aecf63cf08c7baa955f718f
Liwei Feng <liweeiif@gmail.com>
2023-02-15 00:34:11 -0700

    s

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 function App() {
   return (
     <>
-      <Router>
+      <Router basename="/react">
         <Navbar />
         <Switch>
           <Route path='/' exact component={Home} />
