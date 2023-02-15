9ab7491bd68c787ae791ce4f59d017d30ef3e604
Liwei Feng <liweeiif@gmail.com>
2023-02-14 17:42:14 -0700

    Updates

---
+++ precache-manifest.e97c25c885755448b924b284233f774f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d1c2ab9844d33732da3469348c011c1",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "887d5d5ed0e63304407a",
+    "url": "/react/static/css/main.9a0cae38.chunk.css"
+  },
+  {
+    "revision": "45fd5836b82a21efa67c",
+    "url": "/react/static/js/2.d8eb43be.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/react/static/js/2.d8eb43be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "887d5d5ed0e63304407a",
+    "url": "/react/static/js/main.6df0fe84.chunk.js"
+  },
+  {
+    "revision": "0b14bea055d1fd373296",
+    "url": "/react/static/js/runtime-main.b2bc8604.js"
+  }
+]);
